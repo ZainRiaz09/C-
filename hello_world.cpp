e527beacd1129a800ea9ed44d2e1cc633a1be875
Zain Riaz <149959596+ZainRiaz09@users.noreply.github.com>
2024-02-24 00:39:34 +0500

    Add files via upload

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,56 +2,56 @@
 using namespace std;
 
 int main (){
-//     cout << "hello";
+    cout << "hello";
  
-//  int a, b ;
-//  cin>>a>>b;
-//  cout << a+b << endl;
+ int a, b ;
+ cin>>a>>b;
+ cout << a+b << endl;
 
 
-// int amount1;
-// int amount2;
+int amount1;
+int amount2;
 
-// cin>>amount1;
-// cin>>amount2;
+cin>>amount1;
+cin>>amount2;
 
-// int sum = amount1 + amount2;
-// cout<<"sum of this sum is "<<sum;
+int sum = amount1 + amount2;
+cout<<"sum of this sum is "<<sum;
 
-//  int saving;
+ int saving;
 
-//  cin>>saving;
-//  if(saving>1000){
-//     cout<< "ch";
-//  } else {
-//     cout<< "sc";
-//  }
+ cin>>saving;
+ if(saving>1000){
+    cout<< "ch";
+ } else {
+    cout<< "sc";
+ }
 
 
-// int a,b,c;
+int a,b,c;
 
-// cin>>a>>b>>c;
+cin>>a>>b>>c;
 
-// if (a>b){
-//     if (a>c)
-//     {
-//         cout<<a;
-//     }else {
-//         cout<< c;
-//     }
+if (a>b){
+    if (a>c)
+    {
+        cout<<a;
+    }else {
+        cout<< c;
+    }
     
     
 
-//     }else
-//     {
-//         if(b>c){
-//             cout<<b;
-//         }else
-//     {
-//         cout<<c;
-//     }
+    }else
+    {
+        if(b>c){
+            cout<<b;
+        }else
+    {
+        cout<<c;
+    }
     
-//     };
+    };
     
 
 int n;
